2ce25779aec15b1ece248e65e513d0d373e659b3
Renan Silva <renansilvaytB@gmail.com>
2023-08-17 18:56:47 -0300

    feat cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 require('dotenv/config')
 require('express-async-errors')
 
+const express = require('express')
+const AppError = require('./utils/AppError')
+const migrationsRun = require('./database/sqlite/migrations')
+const uploadConfig = require('./configs/upload')
+
+
+const cors = require('cors')
+//const routes = require('./routes')
+
+migrationsRun()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
 app.use(routes)
 
 app.use((error, req, res, next) => {
