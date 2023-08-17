1cd492f0712fc50b89a1c78acbc5e44bd99ced3c
Renan Silva <renansilvaytB@gmail.com>
2023-08-17 18:49:10 -0300

    feat sessionscontroller

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -3,3 +3,10 @@ const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
 const { sign } = require("jsonwebtoken");
 const authConfig = require("../configs/auth");
+
+/**
+ * Classe responsável por lidar com a sessão do usuário
+ */
+class SessionsController {
+
+}
