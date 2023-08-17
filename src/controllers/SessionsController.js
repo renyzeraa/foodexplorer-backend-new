89da3b48c5b3a7b46f175519beb3437587af9d8f
Renan Silva <renansilvaytB@gmail.com>
2023-08-17 18:48:08 -0300

    feat controllers

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,5 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+const { sign } = require("jsonwebtoken");
+const authConfig = require("../configs/auth");
