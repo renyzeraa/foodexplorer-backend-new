2db58f0e0ae2b93281af01a2e2411a17318e6d75
Renan Silva <renansilvaytB@gmail.com>
2023-08-17 18:50:03 -0300

    feat create sessions

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -9,4 +9,34 @@ const authConfig = require("../configs/auth");
  */
 class SessionsController {
 
+  /**
+   * Cria e valida a sessão
+   * @param {Object} request 
+   * @param {Object} response 
+   * @returns JSON
+   */
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+
+    if (!user) {
+      throw new AppError("E-mail está incorreto", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new AppError("Senha está incorreta", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({ user }, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.status(201).json({ user, token });
+  }
 }
