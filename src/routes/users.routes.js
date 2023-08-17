7e1ebcfbdf846bd17fbc7174c3233ee9b5796ad6
Renan Silva <renansilvaytB@gmail.com>
2023-08-17 18:59:16 -0300

    user routes

---
+++ src/routes/users.routes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+
+const usersRoutes = Router();
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.get("/:email", usersController.show);
+
+module.exports = usersRoutes;
