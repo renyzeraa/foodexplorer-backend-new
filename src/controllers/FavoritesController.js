a330417f2ef9aba0345acab7e35636191ea8f75b
Renan Silva <renansilvaytB@gmail.com>
2023-08-17 21:22:21 -0300

    refactor code

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -2,83 +2,107 @@ const knex = require('../database/knex')
 
 class FavoritesController {
   async create(request, response) {
-    const user_id = request.user.id
-    const { plate_id } = request.params
-
-    const plate = await knex('plates').where({ id: plate_id }).first()
+    const user_id = request.user.id;
+    const { plate_id } = request.params;
+  
+    const plate = await this.getPlateById(plate_id);
+  
     if (!plate) {
-      return response
-        .status(404)
-        .json({ error: 'Prato não encontrado' })
+      return this.errorResponse(response, 404, 'Prato não encontrado');
     }
-
+  
+    const isAlreadyFavorite = await this.checkIfPlateIsFavorite(user_id, plate_id);
+  
+    if (isAlreadyFavorite) {
+      return this.errorResponse(response, 400, 'Prato já marcado como favorito');
+    }
+  
+    await this.markPlateAsFavorite(user_id, plate_id);
+  
+    return response.status(201).json({ message: 'Prato marcado como favorito' });
+  }
+  
+  async getPlateById(plate_id) {
+    return await knex('plates').where({ id: plate_id }).select('*').first();
+  }
+  
+  async checkIfPlateIsFavorite(user_id, plate_id) {
     const [existingFavorite] = await knex('favorite_plates')
       .where({ user_id, plate_id })
-      .count('* as count')
-
-    if (existingFavorite.count > 0) {
-      return response
-        .status(400)
-        .json({ error: 'Prato já marcado como favorito' })
-    }
-
-    await knex('favorite_plates').insert({ user_id, plate_id })
-
-    return response.status(201).json({ message: 'Prato marcado como favorito' })
+      .count('* as count');
+  
+    return existingFavorite.count > 0;
   }
-
+  
+  async markPlateAsFavorite(user_id, plate_id) {
+    await knex('favorite_plates').insert({ user_id, plate_id });
+  }
+  
+  
+  async findFavoritePlate(user_id, plate_id) {
+    return await knex('favorite_plates')
+    .where({ user_id, plate_id })
+    .select('plate_id')
+    .first();
+  }
+  
   async show(request, response) {
-    const user_id = request.user.id
-    const { plate_id } = request.params
-
-    const favoritePlate = await knex('favorite_plates')
-      .where({ user_id, plate_id })
-      .select('plate_id')
-      .first()
-
+    const user_id = request.user.id;
+    const { plate_id } = request.params;
+    
+    const favoritePlate = await this.findFavoritePlate(user_id, plate_id);
+    
     if (!favoritePlate) {
-      return response
-        .status(404)
-        .json({ error: 'Prato favoritado, não encontrado' })
+      return this.errorResponse(response, 404, 'Prato favoritado, não encontrado');
     }
-
-    const plate = await knex('plates').where('id', plate_id).select('*').first()
-
-    return response.json(plate)
+    
+    const plate = await this.getPlateById(plate_id);
+    
+    return response.json(plate);
   }
-
+  
   async index(request, response) {
-    const user_id = request.user.id
-
-    const favoritePlates = await knex('favorite_plates')
-      .select('plate_id')
-      .where('favorite_plates.user_id', user_id)
-
-    const plateIds = favoritePlates.map(favorite => favorite.plate_id)
-
-    const plates = await knex('plates').whereIn('id', plateIds).select('*')
-
-    return response.json(plates)
+    const user_id = request.user.id;
+    
+    const favoritePlateIds = await this.getFavoritePlateIds(user_id);
+    const plates = await this.getPlatesByIds(favoritePlateIds);
+    
+    return response.json(plates);
   }
-
+  
+  async getFavoritePlateIds(user_id) {
+    const favoritePlates = await knex('favorite_plates')
+    .select('plate_id')
+    .where('favorite_plates.user_id', user_id);
+    
+    return favoritePlates.map(favorite => favorite.plate_id);
+  }
+  
+  async getPlatesByIds(plateIds) {
+    return await knex('plates').whereIn('id', plateIds).select('*');
+  }
+  
+  async removePlateFromFavorites(user_id, plate_id) {
+    await knex('favorite_plates').where({ user_id, plate_id }).del();
+  }
+  
   async delete(request, response) {
-    const user_id = request.user.id
-    const plate_id = request.params.plate_id
-    const [existingFavorite] = await knex('favorite_plates')
-      .where({ user_id, plate_id: plate_id })
-      .count('* as count')
-
-    if (existingFavorite.count === 0) {
-      return response
-        .status(400)
-        .json({ error: 'Prato não está marcado como favorito' })
+    const user_id = request.user.id;
+    const plate_id = request.params.plate_id;
+    
+    const isFavorite = await this.checkIfPlateIsFavorite(user_id, plate_id);
+    
+    if (!isFavorite) {
+      return this.errorResponse(response, 400, 'Prato não está marcado como favorito');
     }
-
-    await knex('favorite_plates').where({ user_id, plate_id }).del()
-
-    return response
-      .status(200)
-      .json({ message: 'Prato removido dos favoritos' })
+  
+    await this.removePlateFromFavorites(user_id, plate_id);
+    
+    return response.status(200).json({ message: 'Prato removido dos favoritos' });
+  }
+  
+  errorResponse(response, status, message) {
+    return response.status(status).json({ error: message });
   }
 }
 
