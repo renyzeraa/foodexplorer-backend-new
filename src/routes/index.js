1bbd5c130340fed86c64cb6b52142bdd0952bd58
Renan Silva <renansilvaytB@gmail.com>
2023-08-21 19:28:37 -0300

    refactor index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,20 @@
 const { Router } = require("express");
 
-const usersRoutes = require("./users.routes");
-const platesRoutes = require("./plates.routes");
-const favoriteRoutes = require("./favorite.routes");
-const sessionsRoutes = require("./sessions.routes");
-const ingredientsRoutes = require("./ingredients.routes");
-const ordersRoutes = require("./orders.routes");
-const shoppingRoutes = require("./shopping.routes");
-
 const routes = Router();
 
-routes.use("/users", usersRoutes);
-routes.use("/sessions", sessionsRoutes);
-routes.use("/plates", platesRoutes);
-routes.use("/favorites", favoriteRoutes);
-routes.use("/ingredients", ingredientsRoutes);
-routes.use("/orders", ordersRoutes);
-routes.use("/shopping", shoppingRoutes);
+const routeModules = [
+  "users",
+  "plates",
+  "sessions",
+  "favorites",
+  "ingredients",
+  "orders",
+  "shopping"
+];
 
+routeModules.forEach(module => {
+  const moduleRoutes = require(`./${module}.routes`);
+  routes.use(`/${module}`, moduleRoutes);
+});
 
 module.exports = routes;
