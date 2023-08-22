71fa9879f1541f4fe6bc8fe7b30b1cfd2d93f312
Renan Silva <renansilvaytB@gmail.com>
2023-08-22 18:55:52 -0300

    fix code and feat docs

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -2,8 +2,19 @@ const fs = require("fs");
 const path = require("path");
 const uploadConfig = require("../configs/upload");
 
+/**
+ * Classe que gerencia o armazenamento de arquivos no sistema de arquivos local.
+ */
 class DiskStorage {
+
+  /**
+   * Salva um arquivo no sistema de arquivos local.
+   * @param {string} file - O nome do arquivo a ser salvo.
+   * @returns {Promise<string>} - O nome do arquivo salvo.
+   * @throws {Error} - Lança um erro se a operação de renomeação falhar.
+   */
   async saveFile(file) {
+    // Renomeia o arquivo da pasta temporária para a pasta de upload permanente.
     await fs.promises.rename(
       path.resolve(uploadConfig.TMP_FOLDER, file),
       path.resolve(uploadConfig.UPLOAD_FOLDER, file),
@@ -11,6 +22,11 @@ class DiskStorage {
     return file;
   }
 
+  /**
+   * Exclui um arquivo do sistema de arquivos local.
+   * @param {string} file - O nome do arquivo a ser excluído.
+   * @returns {Promise<boolean>} - `true` se o arquivo foi excluído com sucesso, `false` se o arquivo não existe.
+   */
   async deleteFile(file) {
     const filePath = path.resolve(uploadConfig.UPLOAD_FOLDER, file);
 
@@ -18,9 +34,10 @@ class DiskStorage {
       await fs.promises.stat(filePath);
     } catch (e) {
       console.error("Error =>", e);
-      return false;
+      return false; 
     }
     await fs.promises.unlink(filePath);
+    return true;// O arquivo foi excluído com sucesso.
   }
 }
 
