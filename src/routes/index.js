e05f8823521dd02eadeeb16942d4ad7673c37282
Renan Silva <renansilvaytB@gmail.com>
2023-08-22 18:57:05 -0300

    feat docs

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 
+// Cria um objeto Router para lidar com as rotas.
 const routes = Router();
-
+// Lista de módulos de rotas disponíveis no aplicativo.
 const routeModules = [
   "users",
   "plates",
@@ -12,8 +13,11 @@ const routeModules = [
   "shopping"
 ];
 
+// Itera sobre cada módulo e incorpora suas rotas ao aplicativo principal.
 routeModules.forEach(module => {
+  // Importa as rotas específicas para o módulo atual.
   const moduleRoutes = require(`./${module}.routes`);
+  // Define uma rota base para o módulo e anexa as rotas específicas a ela.
   routes.use(`/${module}`, moduleRoutes);
 });
 
