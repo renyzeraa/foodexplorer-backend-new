0a160a5e9a0fb84bc31f2f72132545f6f03c2d96
Renan Silva <renansilvaytB@gmail.com>
2023-08-22 18:18:08 -0300

    feat .env

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.use((error, req, res, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
   console.log(`Server is running on port => ${PORT}`)
 })
